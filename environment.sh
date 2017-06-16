7b1fedd45b0a3c018249dd5fd9a65f88b76f3467
Vytis Šibonis <vytis@wire.com>
2017-06-16 18:22:13 +0200

    Maybe put it locally

--- environment.sh
+++ environment.sh
@@ -1,4 +1,4 @@
-echo "export DESTINATION='platform=iOS Simulator,name=iPhone 6,OS=10.3'" >> ${CIRCLECI_HOME}/.circlerc
-echo "export SCHEME=$(xcodebuild -list | awk '/^[[:space:]]*Schemes:/{getline; print $0;}' | tr -d '[:space:]') " >> ${CIRCLECI_HOME}/.circlerc
+echo "export DESTINATION='platform=iOS Simulator,name=iPhone 6,OS=10.3'" >> .circlerc
+echo "export SCHEME=$(xcodebuild -list | awk '/^[[:space:]]*Schemes:/{getline; print $0;}' | tr -d '[:space:]') " >> .circlerc
 ALL_PROJECTS=( *.xcodeproj )
-echo "export PROJECT='${ALL_PROJECTS[0]}'"
+echo "export PROJECT='${ALL_PROJECTS[0]}'" >> .circlerc
