fec1eeacae274bb1537c2237396fe8eb0ec05e01
Vytis Šibonis <vytis@wire.com>
2017-06-16 17:01:36 +0200

    Added test and post_test

--- post_test.sh
+++ post_test.sh
@@ -1,3 +1,7 @@
 #! /bin/sh
 
 set -euf -o pipefail
+
+SCHEME=$(xcodebuild -list | awk '/^[[:space:]]*Schemes:/{getline; print $0;}' | tr -d '[:space:]') 
+
+bundle exec slather coverage --html --scheme "${SCHEME}" --output-directory $CIRCLE_TEST_REPORTS/code_coverage/ *.xcodeproj
