7f7ba160bc33462a64aa9ac19cb3bb96186fda08
Vytis Šibonis <vytis@wire.com>
2017-06-16 16:47:03 +0200

    Added build step

--- build.sh
+++ build.sh
@@ -2,3 +2,6 @@
 
 set -euf -o pipefail
 
+SCHEME=$(xcodebuild -list | awk '/^[[:space:]]*Schemes:/{getline; print $0;}' | tr -d '[:space:]') 
+
+xcodebuild -scheme "${SCHEME}" build-for-testing -enableCodeCoverage YES -destination "${DESTINATION}" | tee $CIRCLE_ARTIFACTS/xcode_build.log | xcpretty
