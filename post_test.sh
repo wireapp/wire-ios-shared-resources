aa36d2665a1c2962f129975c98041512e9c2390e
Vytis Šibonis <vytis@wire.com>
2017-06-19 10:06:01 +0200

    Added danger

--- post_test.sh
+++ post_test.sh
@@ -4,6 +4,8 @@ set -euf -o pipefail
 
 bundle exec slather coverage --html --scheme "${SCHEME}" --output-directory $CIRCLE_TEST_REPORTS/code_coverage/ $PROJECT
 
+bundle exec danger
+
 mkdir "${CIRCLE_ARTIFACTS}/scripts"
 cp *.sh "${CIRCLE_ARTIFACTS}/scripts"
 cp "${HOME}/.circlerc" "${CIRCLE_ARTIFACTS}/scripts"
