70677dcca509021caa78075e03212edfa31326ed
Vytis Šibonis <vytis@wire.com>
2017-06-19 10:27:05 +0200

    Failing if any one of the steps fail

--- post_test.sh
+++ post_test.sh
@@ -1,11 +1,13 @@
 #! /bin/sh
 
-bundle exec slather coverage --html --scheme "${SCHEME}" --output-directory $CIRCLE_TEST_REPORTS/code_coverage/ $PROJECT
+EXIT=0
+# bundle exec slather coverage --html --scheme "${SCHEME}" --output-directory $CIRCLE_TEST_REPORTS/code_coverage/ $PROJECT
 
-echo "bundle exec slather coverage --html --scheme ${SCHEME} --output-directory ${CIRCLE_TEST_REPORTS}/code_coverage/ ${PROJECT}"
+bundle exec danger || EXIT=$?
 
-bundle exec danger
+mkdir "${CIRCLE_ARTIFACTS}/scripts" || EXIT=$?
+cp *.sh "${CIRCLE_ARTIFACTS}/scripts" || EXIT=$?
+cp "${HOME}/.circlerc" "${CIRCLE_ARTIFACTS}/scripts" || EXIT=$?
 
-mkdir "${CIRCLE_ARTIFACTS}/scripts"
-cp *.sh "${CIRCLE_ARTIFACTS}/scripts"
-cp "${HOME}/.circlerc" "${CIRCLE_ARTIFACTS}/scripts"
+# Fail if any of the commands had exit status != 0
+exit $EXIT
