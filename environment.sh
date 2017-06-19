0b8128c3dd10e71ddee2c16c59d4631c34e039ef
Vytis Šibonis <vytis@wire.com>
2017-06-19 09:59:44 +0200

    Writing to a differenc .circlerc

--- environment.sh
+++ environment.sh
@@ -1,4 +1,4 @@
-echo "export DESTINATION='platform=iOS Simulator,name=iPhone 6,OS=10.3'" >> .circlerc
-echo "export SCHEME=$(xcodebuild -list | awk '/^[[:space:]]*Schemes:/{getline; print $0;}' | tr -d '[:space:]') " >> .circlerc
+echo "export DESTINATION='platform=iOS Simulator,name=iPhone 6,OS=10.3'" >> $HOME/.circlerc
+echo "export SCHEME=$(xcodebuild -list | awk '/^[[:space:]]*Schemes:/{getline; print $0;}' | tr -d '[:space:]') " >> $HOME/.circlerc
 ALL_PROJECTS=( *.xcodeproj )
-echo "export PROJECT='${ALL_PROJECTS[0]}'" >> .circlerc
+echo "export PROJECT='${ALL_PROJECTS[0]}'" >> $HOME/.circlerc
