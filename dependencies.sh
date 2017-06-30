830c05e80cef072f4fd5b088049735d8317d15b1
Vytis Šibonis <vytis@wire.com>
2017-06-30 10:16:13 +0200

    Downloading gemfile before installing

--- dependencies.sh
+++ dependencies.sh
@@ -2,5 +2,11 @@
 
 set -euf -o pipefail
 
+echo "Downloading ${DEPENDENCIES_BASE_URL}/Gemfile"
+curl -O "${DEPENDENCIES_BASE_URL}/Gemfile"
+
+echo "Downloading ${DEPENDENCIES_BASE_URL}/Gemfile.lock"
+curl -O "${DEPENDENCIES_BASE_URL}/Gemfile.lock"
+
 bundle install --path ~/.gem
 carthage bootstrap
