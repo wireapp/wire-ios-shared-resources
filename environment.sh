1c14c463bd1a8d5912b320008ddb200fa4a1c98b
Vytis <vytis@proto.lt>
2017-06-30 10:22:05 +0200

    Update environment.sh

--- environment.sh
+++ environment.sh
@@ -1,4 +1,4 @@
-echo "export DESTINATION='platform=iOS Simulator,name=iPhone 6,OS=10.3.1'" >> $HOME/.circlerc
+echo "export DESTINATION='platform=iOS Simulator,name=iPhone 6,OS=10.3'" >> $HOME/.circlerc
 echo "export SCHEME=\"$(xcodebuild -list | awk '/^[[:space:]]*Schemes:/{getline; print $0;}' | sed 's/^ *//;s/ *$//')\"" >> $HOME/.circlerc
 ALL_PROJECTS=( *.xcodeproj )
 echo "export PROJECT='${ALL_PROJECTS[0]}'" >> $HOME/.circlerc
