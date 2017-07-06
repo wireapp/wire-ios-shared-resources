84a9206807efe8f3795832da7a364725d272967c
Vytis Šibonis <vytis@wire.com>
2017-07-06 12:10:53 +0200

    Manually specifying scheme for UI project

--- environment.sh
+++ environment.sh
@@ -1,6 +1,13 @@
 #! /bin/bash
 
 echo "export DESTINATION='platform=iOS Simulator,name=iPhone 6,OS=10.2'" >> $HOME/.circlerc
-echo "export SCHEME=\"$(xcodebuild -list | awk '/^[[:space:]]*Schemes:/{getline; print $0;}' | sed 's/^ *//;s/ *$//')\"" >> $HOME/.circlerc
+
+if [ "$CIRCLE_PROJECT_REPONAME" == "wire-ios"]; 
+then
+	echo "export SCHEME=Wire-iOS" >> $HOME/.circlerc
+else
+	echo "export SCHEME=\"$(xcodebuild -list | awk '/^[[:space:]]*Schemes:/{getline; print $0;}' | sed 's/^ *//;s/ *$//')\"" >> $HOME/.circlerc
+fi
+
 ALL_PROJECTS=( *.xcodeproj )
 echo "export PROJECT='${ALL_PROJECTS[0]}'" >> $HOME/.circlerc
