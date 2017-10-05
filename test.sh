c4714909d5043424c49f827f0fa3f730336b5bfd
Vytis Šibonis <vytis@wire.com>
2017-10-05 15:09:42 +0200

    Same format for test results and logs for UI project as well

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ set -euf -o pipefail
 
 if [ "$CIRCLE_PROJECT_REPONAME" == "wire-ios" ]; 
 then
-  xcodebuild -workspace "Wire-iOS.xcworkspace" -scheme "${SCHEME}" -enableCodeCoverage YES -destination "${DESTINATION}" test-without-building | tee "${CIRCLE_ARTIFACTS}/xcode_build.log" | xcpretty
+  xcodebuild -workspace "Wire-iOS.xcworkspace" -scheme "${SCHEME}" -enableCodeCoverage YES -destination "${DESTINATION}" test-without-building | tee "${CIRCLE_ARTIFACTS}/xcode_test.log" | xcpretty -r junit --output $CIRCLE_TEST_REPORTS/junit/tests.xml
 else
   xcodebuild -scheme "${SCHEME}" -enableCodeCoverage YES -destination "${DESTINATION}" test-without-building | tee $CIRCLE_ARTIFACTS/xcode_test.log | xcpretty -r junit --output $CIRCLE_TEST_REPORTS/junit/tests.xml
 fi
