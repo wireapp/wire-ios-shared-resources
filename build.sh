a2c80b7c449fa5cc25b26065fc63f0b8b1c50f18
Vytis Šibonis <vytis@wire.com>
2017-10-26 18:31:51 +0200

    wip

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 
 set -euf -o pipefail
 
+source $HOME/.circlerc
+
 if [ "$CIRCLE_PROJECT_REPONAME" == "wire-ios" ]; 
 then
   xcodebuild -workspace "Wire-iOS.xcworkspace" -scheme "${SCHEME}" -enableCodeCoverage YES -destination "${DESTINATION}" build-for-testing | tee "${CIRCLE_ARTIFACTS}/xcode_build.log" | xcpretty
