1fca692c58c91670a23bb5cad57824c3b9631dfe
Vytis Šibonis <vytis@wire.com>
2017-10-26 19:02:05 +0200

    wip

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ source env-vars
 
 if [ "$CIRCLE_PROJECT_REPONAME" == "wire-ios" ]; 
 then
-  xcodebuild -workspace "Wire-iOS.xcworkspace" -scheme "${SCHEME}" -enableCodeCoverage YES -destination "${DESTINATION}" build-for-testing | tee "xcode_build.log" | xcpretty
+  xcodebuild -workspace "Wire-iOS.xcworkspace" -scheme "${SCHEME}" -enableCodeCoverage YES -destination "${DESTINATION}" -derivedDataPath DerivedData build-for-testing | tee "xcode_build.log" | xcpretty
 else
   xcodebuild -scheme "${SCHEME}" -enableCodeCoverage YES -destination "${DESTINATION}" build-for-testing | tee "xcode_build.log" | xcpretty
 fi
