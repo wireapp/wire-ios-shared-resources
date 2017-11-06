371d2e2ace660d0b7bce8bff6d37aef4d69c8223
Vytis Šibonis <vytis@wire.com>
2017-11-06 13:48:38 +0100

    Analyzing on build

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -euf -o pipefail
 
 if [ "$CIRCLE_PROJECT_REPONAME" == "wire-ios" ]; 
 then
-  xcodebuild -workspace "Wire-iOS.xcworkspace" -scheme "${SCHEME}" -enableCodeCoverage YES -destination "${DESTINATION}" build-for-testing | tee "${CIRCLE_ARTIFACTS}/xcode_build.log" | xcpretty
+  xcodebuild -workspace "Wire-iOS.xcworkspace" -scheme "${SCHEME}" -enableCodeCoverage YES -destination "${DESTINATION}" analyze build-for-testing | tee "${CIRCLE_ARTIFACTS}/xcode_build.log" | xcpretty
 else
-  xcodebuild -scheme "${SCHEME}" -enableCodeCoverage YES -destination "${DESTINATION}" build-for-testing | tee "${CIRCLE_ARTIFACTS}/xcode_build.log" | xcpretty
+  xcodebuild -scheme "${SCHEME}" -enableCodeCoverage YES -destination "${DESTINATION}" analyze build-for-testing | tee "${CIRCLE_ARTIFACTS}/xcode_build.log" | xcpretty
 fi
