fa560016441a0b60161fb226dbb2b8a00ad8706d
Vytis Šibonis <vytis@wire.com>
2017-12-08 14:34:59 +0100

    wip

--- post_test.sh
+++ post_test.sh
@@ -6,14 +6,6 @@ echo "Downloading ${DEPENDENCIES_BASE_URL}/Dangerfile"
 curl -O "${DEPENDENCIES_BASE_URL}/Dangerfile" || EXIT=$?
 bundle exec danger || EXIT=$?
 
-echo "Creating directory for all scripts"
-mkdir "${CIRCLE_ARTIFACTS}/scripts" || EXIT=$?
-
-echo "Copying all shell scripts"
-cp *.sh "${CIRCLE_ARTIFACTS}/scripts" || EXIT=$?
-
-if [ -d "SnapshotResults" ]; then echo "Copying snapshot results"; cp -R SnapshotResults $CIRCLE_ARTIFACTS/ || EXIT=$?; fi
-
 if [ "$CIRCLE_PROJECT_REPONAME" == "wire-ios" ]; 
 then
   echo "Skipping code coverage for UI project"
