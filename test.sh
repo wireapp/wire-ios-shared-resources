612b51408f959f0725c2571a13082e78d8c79dfb
Vytis Šibonis <vytis@wire.com>
2017-12-12 10:14:06 +0100

    wip

--- test.sh
+++ test.sh
@@ -4,9 +4,11 @@ set -euf -o pipefail
 
 bundle install --path ~/.gem
 
+XCRUN_FILES=( DerivedData/Build/Products/*.xctestrun )
+
 if [ "$CIRCLE_PROJECT_REPONAME" == "wire-ios" ]; 
 then
   xcodebuild -workspace "Wire-iOS.xcworkspace" -scheme "${SCHEME}" -enableCodeCoverage YES -destination "${DESTINATION}" -derivedDataPath DerivedData test-without-building | tee "xcode_test.log" | bundle exec xcpretty -r junit --output junit/tests.xml
 else
-  xcodebuild -enableCodeCoverage YES -destination "${DESTINATION}" -xctestrun DerivedData/Build/Products/*.xctestrun test-without-building | tee xcode_test.log | bundle exec xcpretty -r junit --output junit/tests.xml
+  xcodebuild -enableCodeCoverage YES -destination "${DESTINATION}" -xctestrun "${XCRUN_FILES[0]}" test-without-building | tee xcode_test.log | bundle exec xcpretty -r junit --output junit/tests.xml
 fi
