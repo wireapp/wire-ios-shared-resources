57ef382854c6cb02841f32b63d480074f5ecad41
Vytis Šibonis <vytis@wire.com>
2017-12-12 14:09:04 +0100

    wip

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -euf -o pipefail
 
 if [ "$CIRCLE_PROJECT_REPONAME" == "wire-ios" ]; 
 then
-  xcodebuild -workspace "Wire-iOS.xcworkspace" -scheme "${SCHEME}" -destination "${DESTINATION}" -derivedDataPath DerivedData build-for-testing | tee "xcode_build.log" | XCPRETTY_JSON_FILE_OUTPUT=xcodebuild.json bundle exec xcpretty -f `bundle exec xcpretty-json-formatter`
+  xcodebuild -workspace "Wire-iOS.xcworkspace" -scheme "${SCHEME}" -destination "${DESTINATION}" -derivedDataPath DerivedData build-for-testing | XCPRETTY_JSON_FILE_OUTPUT=xcodebuild.json bundle exec xcpretty -f `bundle exec xcpretty-json-formatter`
 else
-  xcodebuild -scheme "${SCHEME}" -enableCodeCoverage YES -destination "${DESTINATION}" -derivedDataPath DerivedData analyze build-for-testing | tee "xcode_build.log" | XCPRETTY_JSON_FILE_OUTPUT=xcodebuild.json bundle exec xcpretty -f `bundle exec xcpretty-json-formatter`
+  xcodebuild -scheme "${SCHEME}" -enableCodeCoverage YES -destination "${DESTINATION}" -derivedDataPath DerivedData analyze build-for-testing | XCPRETTY_JSON_FILE_OUTPUT=xcodebuild.json bundle exec xcpretty -f `bundle exec xcpretty-json-formatter`
 fi
