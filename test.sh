e5200ed1517bbf77c94aa04da8f8b6d4457d5e50
Vytis Šibonis <vytis@wire.com>
2017-12-13 10:22:15 +0100

    Falling back to old testing for UI project

--- test.sh
+++ test.sh
@@ -4,6 +4,14 @@ set -eu -o pipefail
 
 bundle install --path ~/.gem
 
-XCRUN_FILES=( DerivedData/Build/Products/*.xctestrun )
+if [ "$IS_UI_PROJECT" -eq "1" ]; 
+then
+	# Running tests from .xctestrun file seems broken, falling back to regular command
+	xcodebuild -destination "${DESTINATION}" -workspace "${WORKSPACE}" -scheme "${SCHEME}" -derivedDataPath DerivedData test-without-building | tee xcode_test.log | bundle exec xcpretty -r junit --output junit/tests.xml
+else
+	XCRUN_FILES=( DerivedData/Build/Products/*.xctestrun )
 
-xcodebuild -enableCodeCoverage YES -destination "${DESTINATION}" -xctestrun "${XCRUN_FILES[0]}" test-without-building | tee xcode_test.log | bundle exec xcpretty -r junit --output junit/tests.xml
+	xcodebuild -enableCodeCoverage YES -destination "${DESTINATION}" -xctestrun "${XCRUN_FILES[0]}" test-without-building | tee xcode_test.log | bundle exec xcpretty -r junit --output junit/tests.xml
+fi
+
+
