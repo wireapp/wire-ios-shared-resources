c38be649c43c199da579a7cb52ccf2d0df4ec78a
Vytis Šibonis <vytis@wire.com>
2017-12-13 11:24:09 +0100

    Not using xctestrun file anymore as it doesn’t give coverage data

--- test.sh
+++ test.sh
@@ -4,14 +4,17 @@ set -eu -o pipefail
 
 bundle install --path ~/.gem
 
+arguments=( -scheme "${SCHEME}"  )
+arguments+=( -destination "${DESTINATION}" )
+arguments+=( -derivedDataPath DerivedData )
+
 if [ "$IS_UI_PROJECT" -eq "1" ]; 
 then
-	# Running tests from .xctestrun file seems broken, falling back to regular command
-	xcodebuild -destination "${DESTINATION}" -workspace "${WORKSPACE}" -scheme "${SCHEME}" -derivedDataPath DerivedData test-without-building | tee xcode_test.log | bundle exec xcpretty -r junit --output junit/tests.xml
+	arguments+=( -workspace "${WORKSPACE}" ) # We need to append workspace argument for UI project
 else
-	XCRUN_FILES=( DerivedData/Build/Products/*.xctestrun )
-
-	xcodebuild -enableCodeCoverage YES -destination "${DESTINATION}" -xctestrun "${XCRUN_FILES[0]}" test-without-building | tee xcode_test.log | bundle exec xcpretty -r junit --output junit/tests.xml
+	arguments+=( -enableCodeCoverage YES ) # Track code coverage in frameworks
 fi
 
+arguments+=( test-without-building )
 
+xcodebuild "${arguments[@]}" | tee xcode_test.log | bundle exec xcpretty -r junit --output junit/tests.xml
