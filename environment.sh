3480a6128e511f3cc1bd322b6b14669503162250
Vytis Šibonis <vytis@wire.com>
2017-12-13 10:22:05 +0100

    Removed ruby version

--- environment.sh
+++ environment.sh
@@ -2,7 +2,6 @@
 
 echo "export DESTINATION='platform=iOS Simulator,name=iPhone 7,OS=11.0.1'" >> $BASH_ENV
 
-curl -O "${DEPENDENCIES_BASE_URL}/.ruby-version"
 curl -O "${DEPENDENCIES_BASE_URL}/dependencies.sh"
 curl -O "${DEPENDENCIES_BASE_URL}/build.sh"
 curl -O "${DEPENDENCIES_BASE_URL}/test.sh"
@@ -15,6 +14,7 @@ then
 	echo "export WORKSPACE='Wire-iOS.xcworkspace'" >> $BASH_ENV
 else
 	echo "export IS_UI_PROJECT=0" >> $BASH_ENV
+	curl -O "${DEPENDENCIES_BASE_URL}/.ruby-version"
 	curl -O "${DEPENDENCIES_BASE_URL}/Gemfile"
 	curl -O "${DEPENDENCIES_BASE_URL}/Gemfile.lock"
 	echo "export SCHEME=\"$(xcodebuild -list | awk '/^[[:space:]]*Schemes:/{getline; print $0;}' | sed 's/^ *//;s/ *$//')\"" >> $BASH_ENV
