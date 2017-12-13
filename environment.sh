d3fd98604475c5c7f23f56485a849ba2618254ef
Vytis Šibonis <vytis@wire.com>
2017-12-13 10:33:17 +0100

    Wip

--- environment.sh
+++ environment.sh
@@ -6,6 +6,7 @@ curl -O "${DEPENDENCIES_BASE_URL}/dependencies.sh"
 curl -O "${DEPENDENCIES_BASE_URL}/build.sh"
 curl -O "${DEPENDENCIES_BASE_URL}/test.sh"
 curl -O "${DEPENDENCIES_BASE_URL}/post_test.sh"
+curl -O "${DEPENDENCIES_BASE_URL}/.ruby-version"
 
 if [ "$CIRCLE_PROJECT_REPONAME" == "wire-ios" ]; 
 then
@@ -14,7 +15,6 @@ then
 	echo "export WORKSPACE='Wire-iOS.xcworkspace'" >> $BASH_ENV
 else
 	echo "export IS_UI_PROJECT=0" >> $BASH_ENV
-	curl -O "${DEPENDENCIES_BASE_URL}/.ruby-version"
 	curl -O "${DEPENDENCIES_BASE_URL}/Gemfile"
 	curl -O "${DEPENDENCIES_BASE_URL}/Gemfile.lock"
 	echo "export SCHEME=\"$(xcodebuild -list | awk '/^[[:space:]]*Schemes:/{getline; print $0;}' | sed 's/^ *//;s/ *$//')\"" >> $BASH_ENV
