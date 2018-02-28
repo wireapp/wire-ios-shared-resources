d1f0ee91a3881f74abf5404ba9c7cb3ed2be959e
Vytis Šibonis <vytis@wire.com>
2018-02-28 16:04:39 +0100

    Enabled code coverage tracking on UI project

--- post_test.sh
+++ post_test.sh
@@ -6,13 +6,8 @@ echo "Downloading ${DEPENDENCIES_BASE_URL}/Dangerfile"
 curl -O "${DEPENDENCIES_BASE_URL}/Dangerfile" || EXIT=$?
 bundle exec danger || EXIT=$?
 
-if [ "$IS_UI_PROJECT" -eq "1" ]; 
-then
-  echo "Skipping code coverage for UI project"
-else
-  echo "Uploading coverage to codecov.io"
-  bash <(curl -s https://codecov.io/bash) -J "^${SCHEME}$" -D DerivedData -X xcodellvm || EXIT=$?
-fi
+echo "Uploading coverage to codecov.io"
+bash <(curl -s https://codecov.io/bash) -J "^${SCHEME}$" -D DerivedData -X xcodellvm || EXIT=$?
 
 # Fail if any of the commands had exit status != 0
 exit $EXIT
