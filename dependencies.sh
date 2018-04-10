cbb3514d726b663a9c2bb905e3ffdf1e1229b511
Vytis <vytis@users.noreply.github.com>
2018-04-10 15:37:44 +0200

    Ignoring carthage if configuration not found

--- dependencies.sh
+++ dependencies.sh
@@ -11,6 +11,10 @@ then
     ./setup.sh
 else
     bundle install --path ~/.gem
-    carthage bootstrap --platform iOS --cache-builds
+    if [ -e "Carfile" ]; then
+        carthage bootstrap --platform iOS --cache-builds
+    else
+        echo "No Carthage found, skipping bootstrap"
+    fi 
 fi
 
