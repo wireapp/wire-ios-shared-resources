4033b369cb2231e837306d7be8452ae2e19c1a41
Alexis Aubry <alexis.aubry@wire.com>
2018-07-04 15:01:05 +0200

    Fix build script not fetching dependencies on UI

--- dependencies.sh
+++ dependencies.sh
@@ -2,8 +2,11 @@
 
 set -euf -o pipefail
 
-if [ "$IS_UI_PROJECT" -ne "1" ];
+if [ "$IS_UI_PROJECT" -eq "1" ]; 
 then
+    echo "Running setup.sh"
+    ./setup.sh
+else
     bundle install --path ~/.gem
     if [ -e "Cartfile" ]; then
         carthage bootstrap --platform iOS --cache-builds
