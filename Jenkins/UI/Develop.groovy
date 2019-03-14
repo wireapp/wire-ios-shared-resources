6092e882ced8953fc4ada1f69a39d051641c03e2
Vytis Šibonis <vytis@wire.com>
2019-03-14 13:41:26 +0100

    Getting last commit from SCM step

--- Jenkins/UI/Develop.groovy
+++ Jenkins/UI/Develop.groovy
@@ -8,23 +8,28 @@ pipeline {
     }
     environment {
         BRANCH = "develop"
+        LAST_COMMIT = ""
     }
     stages {
         stage('Checkout') {
             steps {
-                // Checking out the correct repository. This is dynamic because we have one job that can build multiple frameworks
-                checkout([
-                    $class: 'GitSCM',
-                    branches: [[name: BRANCH]], // Checks out specified branch
-                    extensions: [
-                        [$class: 'AuthorInChangelog'],
-                        [$class: 'CloneOption', noTags: true, reference: '', shallow: true],
-                        [$class: 'CheckoutOption', timeout: 30],
-                        [$class: 'LocalBranch', localBranch: '**'], // Unless this is specified, it simply checks out by commit SHA with no branch information
-                        [$class: 'CleanBeforeCheckout'] // Resets untracked files, just to make sure we are clean
-                    ],
-                    userRemoteConfigs: [[url: "git@github.com:wireapp/wire-ios.git"]]
-                ])
+                script {
+                    // Checking out the correct repository. This is dynamic because we have one job that can build multiple frameworks
+                    def scmVars = checkout([
+                        $class: 'GitSCM',
+                        branches: [[name: BRANCH]], // Checks out specified branch
+                        extensions: [
+                            [$class: 'AuthorInChangelog'],
+                            [$class: 'CloneOption', noTags: true, reference: '', shallow: true],
+                            [$class: 'CheckoutOption', timeout: 30],
+                            [$class: 'LocalBranch', localBranch: '**'], // Unless this is specified, it simply checks out by commit SHA with no branch information
+                            [$class: 'CleanBeforeCheckout'] // Resets untracked files, just to make sure we are clean
+                        ],
+                        userRemoteConfigs: [[url: "git@github.com:wireapp/wire-ios.git"]]
+                    ])
+
+                    LAST_COMMIT = scmVars.GIT_PREVIOUS_COMMIT ?: ""
+                }
             }
         }
         stage ("Trigger build") {
@@ -35,7 +40,7 @@ pipeline {
                         string(name: 'branch_to_build', value: BRANCH), 
                         string(name: 'BUILD_TYPE', value: 'Development'), 
                         string(name: 'build_number_override', value: env.BUILD_NUMBER),
-                        string(name: 'last_commit_for_changelog', value: env.GIT_PREVIOUS_COMMIT ?: "")
+                        string(name: 'last_commit_for_changelog', value: LAST_COMMIT)
                     ]
                 )
             }
