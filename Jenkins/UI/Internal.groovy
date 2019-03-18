c6a1e9d2b11800b39ff24267a2d538c4969f7405
Vytis Šibonis <vytis@wire.com>
2019-03-18 17:49:32 +0100

    Added Internal build

---
+++ Jenkins/UI/Internal.groovy
@@ -0,0 +1,49 @@
+pipeline {
+    agent any
+    options {
+        ansiColor('xterm')
+    }
+    triggers { 
+        pollSCM('H 21 * * *') 
+    }
+    environment {
+        BRANCH = "develop"
+        LAST_COMMIT = ""
+    }
+    stages {
+        stage('Checkout') {
+            steps {
+                script {
+                    def scmVars = checkout([
+                        $class: 'GitSCM',
+                        branches: [[name: BRANCH]], // Checks out specified branch
+                        extensions: [
+                            [$class: 'AuthorInChangelog'],
+                            [$class: 'CloneOption', noTags: true, reference: '', shallow: true],
+                            [$class: 'CheckoutOption', timeout: 30],
+                            [$class: 'LocalBranch', localBranch: '**'], // Unless this is specified, it simply checks out by commit SHA with no branch information
+                            [$class: 'CleanBeforeCheckout'] // Resets untracked files, just to make sure we are clean
+                        ],
+                        userRemoteConfigs: [[url: "git@github.com:wireapp/wire-ios.git"]]
+                    ])
+
+                    LAST_COMMIT = scmVars.GIT_PREVIOUS_COMMIT ?: ""
+                }
+            }
+        }
+        stage ("Trigger build") {
+            steps {
+                build(
+                    job: 'client-ios-build-pipeline', 
+                    parameters: [
+                        string(name: 'branch_to_build', value: BRANCH), 
+                        string(name: 'BUILD_TYPE', value: 'Internal'), 
+                        string(name: 'build_number_override', value: env.BUILD_NUMBER),
+                        string(name: 'last_commit_for_changelog', value: LAST_COMMIT)
+                    ]
+                )
+            }
+        }
+    }
+}
+
