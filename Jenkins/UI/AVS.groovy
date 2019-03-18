cec2a06f80f988c7ce3164e9d9ff93b7537f9bc4
Vytis Šibonis <vytis@wire.com>
2019-03-18 17:49:21 +0100

    Added AVS build

---
+++ Jenkins/UI/AVS.groovy
@@ -0,0 +1,26 @@
+pipeline {
+    agent any
+    options {
+        ansiColor('xterm')
+    }
+    parameters {
+        string(defaultValue: "", description: 'Version of AVS to use', name: 'avs_version')
+        string(defaultValue: "develop", description: 'Branch to use', name: 'branch_to_build')
+    }
+
+    stages {
+        stage ("Trigger build") {
+            steps {
+                build(
+                    job: 'client-ios-build-pipeline', 
+                    parameters: [
+                        string(name: 'branch_to_build', value: branch_to_build), 
+                        string(name: 'BUILD_TYPE', value: 'AVS'), 
+                        string(name: 'avs_version', value: avs_version),
+                        string(name: 'build_number_override', value: env.BUILD_NUMBER)
+                    ]
+                )
+            }
+        }
+    }
+}
