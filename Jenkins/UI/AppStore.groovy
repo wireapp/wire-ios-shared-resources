4f71037ced6e286d9af367537dfbd51463958010
Vytis Šibonis <vytis@wire.com>
2019-04-04 11:27:00 +0200

    Added AppStore trigger job

---
+++ Jenkins/UI/AppStore.groovy
@@ -0,0 +1,56 @@
+pipeline {
+    agent any
+    options {
+        ansiColor('xterm')
+    }
+    triggers { 
+        pollSCM('H/2 * * * *') 
+    }
+    environment {
+        BRANCH = ""
+    }
+    stages {
+        stage('Checkout') {
+            steps {
+                script {
+                    def scmVars = checkout([
+                        $class: 'GitSCM',
+                        branches: [[name: "origin/release/**"]],
+                        extensions: [
+                            [$class: 'AuthorInChangelog'],
+                            [$class: 'CloneOption', depth: 0, honorRefspec: true, noTags: true, reference: ''],
+                            [$class: 'CheckoutOption', timeout: 30],
+                            [$class: 'LocalBranch', localBranch: '**'], // Unless this is specified, it simply checks out by commit SHA with no branch information
+                            [$class: 'CleanBeforeCheckout'] // Resets untracked files, just to make sure we are clean
+                        ],
+                        userRemoteConfigs: [[url: "git@github.com:wireapp/wire-ios.git"]]
+                    ])
+
+                    LAST_COMMIT = scmVars.GIT_PREVIOUS_COMMIT ?: ""
+                    BRANCH = scmVars.GIT_BRANCH
+                }
+            }
+        }
+        stage ("Trigger build") {
+            steps {
+                build(
+                    job: 'client-ios-build-pipeline', 
+                    parameters: [
+                        string(name: 'branch_to_build', value: BRANCH), 
+                        string(name: 'BUILD_TYPE', value: 'RC'), 
+                        string(name: 'build_number_override', value: env.BUILD_NUMBER),
+                        string(name: 'last_commit_for_changelog', value: LAST_COMMIT)
+                    ]
+                )
+                build(
+                    job: 'client-ios-appstore-build-pipeline', 
+                    parameters: [
+                        string(name: 'branch_to_build', value: BRANCH), 
+                        string(name: 'build_number_override', value: env.BUILD_NUMBER),
+                    ]
+                )
+
+            }
+        }
+    }
+}
