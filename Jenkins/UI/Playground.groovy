37405c4965f579a14fff45fef48d316995b98c9a
bill chan <billypchan@gmail.com>
2020-01-22 10:59:53 +0100

    change playground default branch to XCode 11

--- Jenkins/UI/Playground.groovy
+++ Jenkins/UI/Playground.groovy
@@ -4,7 +4,7 @@ pipeline {
         ansiColor('xterm')
     }
     parameters {
-        string(defaultValue: "develop", description: 'Branch to use', name: 'branch_to_build')
+        string(defaultValue: "chore/xcode11.3", description: 'Branch to use', name: 'branch_to_build')
         string(defaultValue: "/Applications/Xcode_11.3.1.app/Contents/Developer", description: 'XCode to use', name: 'developer_dir')
     }
 
