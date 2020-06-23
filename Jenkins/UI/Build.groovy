b56f0ea8b4be92f6e8c114e89485c14e98ec5038
bill chan <billypchan@gmail.com>
2020-06-23 14:50:47 +0200

    sleep before build for QA

--- Jenkins/UI/Build.groovy
+++ Jenkins/UI/Build.groovy
@@ -110,18 +110,24 @@ pipeline {
                 """
             }
         }
-        stage('Test') {
-            steps {
-                sh """#!/bin/bash -l
-                    bundle exec fastlane test xcode_version:${xcode_version}
-                """
-            }
-        }
-        stage("QA: build for simulator") {
-            steps {
-                sh """#!/bin/bash -l
-                    bundle exec fastlane build_for_release build_number:${BUILD_NUMBER} build_type:${BUILD_TYPE} configuration:Debug for_simulator:true xcode_version:${xcode_version}
-                """
+        stage('Test & QA: build for simulator') {
+            parallel {
+                stage('Test') {
+                    steps {
+                        sh """#!/bin/bash -l
+                            bundle exec fastlane test xcode_version:${xcode_version}
+                        """
+                    }
+                }
+                stage("QA: build for simulator") {
+                    steps {
+                        sh """#!/bin/bash -l
+                            ### sleep 2 minute, test takes ~3min and may block the test when zipping the files
+                            sleep 2m
+                            bundle exec fastlane build_for_release build_number:${BUILD_NUMBER} build_type:${BUILD_TYPE} configuration:Debug for_simulator:true xcode_version:${xcode_version}
+                        """
+                    }
+                }
             }
         }
         stage("QA: build for device") {
