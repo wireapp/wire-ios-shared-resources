23e5625bb87fa144d834f0b9f137c0350e6f2904
bill chan <billypchan@gmail.com>
2021-04-14 15:18:49 +0200

    add test plan .groovy

---
+++ Jenkins/UI/TestPlan.groovy
@@ -0,0 +1,136 @@
+@NonCPS
+def getParentBuild() {
+    def upstream = currentBuild.rawBuild.getCause(hudson.model.Cause$UpstreamCause)
+    def upstreamJob = upstream?.upstreamProject ?: env.JOB_NAME
+    def upstreamNumber = upstream?.upstreamBuild ?: env.BUILD_NUMBER
+    return ["name":upstreamJob, "number":upstreamNumber]
+}
+def parentBuild = getParentBuild()
+
+pipeline {
+    agent any
+    options {
+        ansiColor('xterm')
+    }
+    environment { 
+        // This is where the misc configuration files (e.g. Gemfile) are fetched from. When testing swap out the branch
+        DEPENDENCIES_BASE_URL = "https://raw.githubusercontent.com/wireapp/wire-ios-shared-resources/master"
+        // For command line tools to be able to access API we set there env vars to values from Jenkins credentials store
+        GITHUB_TOKEN = credentials('github-api-token')
+        GITHUB_ACCESS_TOKEN = credentials('github-api-token')
+
+
+        // Most fool-proof way to make sure rbenv and ruby works fine
+        PATH = "/Users/ci/.rbenv/shims:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
+
+        // Turn off annoying update checks
+        FASTLANE_SKIP_UPDATE_CHECK = "YES"
+
+        // Repository from which to fetch custom AVS binary
+	    AVS_REPO = "wireapp/avs-ios-binaries-appstore"
+
+        XCODE_VERSION = "${xcode_version}"
+        CACHE_CARTHAGE = "${cache_carthage}"
+    }
+    parameters {
+        string(defaultValue: "develop", description: 'Branch to use', name: 'branch_to_build')
+        string(defaultValue: "", description: 'Version of AVS to use, only relevant for AVS build', name: 'avs_version')
+        string(defaultValue: "999999", description: 'Override build number with', name: 'build_number_override')
+        choice(
+            choices: ["Development"], 
+            description: 'Type of build', 
+            name: "BUILD_TYPE"
+        )
+    }
+
+
+    stages {
+        stage('Checkout') {
+            steps {
+
+                // Checking out the correct repository. This is dynamic because we have one job that can build multiple frameworks
+                checkout([
+                    $class: 'GitSCM',
+                    branches: [[name: '*/${branch_to_build}']], // Checks out specified branch
+                    extensions: [
+                        [$class: 'LocalBranch', localBranch: '**'], // Unless this is specified, it simply checks out by commit SHA with no branch information
+                        [$class: 'CleanBeforeCheckout'], // Resets untracked files, just to make sure we are clean
+                        [$class: 'CloneOption', timeout: 60] // Timeout after 1 hour
+                    ],
+                    userRemoteConfigs: [[url: "git@github.com:wireapp/wire-ios.git"]]
+                ])
+
+            }
+        }
+
+        stage('build-assets & Gems') {
+	        parallel {
+	        	stage('checkout wire-ios-build-assets') {
+		            steps {
+		                dir("wire-ios-build-assets") {
+		                    checkout([
+		                        $class: 'GitSCM',
+		                        branches: [[name: '*/master']], // Checks out specified branch
+		                        extensions: [
+		                            [$class: 'LocalBranch', localBranch: '**'], // Unless this is specified, it simply checks out by commit SHA with no branch information
+		                            [$class: 'CleanBeforeCheckout'] // Resets untracked files, just to make sure we are clean
+		                        ],
+		                        userRemoteConfigs: [[url: "git@github.com:wireapp/wire-ios-build-assets.git"]]
+		                    ])
+		                }
+		            }
+	        	}
+
+	        	stage('Gems') {
+		            steps {
+		                sh """#!/bin/bash -l
+		                    curl -O ${DEPENDENCIES_BASE_URL}/Gemfile
+		                    curl -O ${DEPENDENCIES_BASE_URL}/Gemfile.lock
+  
+		                    bundle install --path ~/.gem
+                                    echo "set DEVELOPER_DIR to XCode 12.4"
+                                    export DEVELOPER_DIR=/Applications/Xcode_12.4.app/Contents/Developer
+		                """
+	            	}  
+	            }
+	        }
+	    }
+
+
+        stage('fastlane prepare') {
+            steps {
+                sh """#!/bin/bash -l
+                    bundle exec fastlane prepare build_number:${BUILD_NUMBER} build_type:${BUILD_TYPE} avs_version:${avs_version} xcode_version:${xcode_version} cache_carthage:${cache_carthage}
+                """
+
+
+                // Make sure that all subsequent steps see the branch from main project, not from build assets
+                script {
+                    GIT_BRANCH = "${branch_to_build}"
+                }
+            }
+        }
+
+        stage('Build') {
+            steps {
+                sh """#!/bin/bash -l
+                    bundle exec fastlane build \
+                     build_number:${BUILD_NUMBER} \
+                     build_type:${BUILD_TYPE} \
+                     configuration:Debug \
+                     for_simulator:true \
+                     xcode_version:${xcode_version}
+                """
+            }
+        }
+
+        stage('Test') {
+            steps {
+                sh """#!/bin/bash -l
+                    bundle exec fastlane test xcode_version:${xcode_version}
+                """
+            }
+        }
+        
+    }
+}
