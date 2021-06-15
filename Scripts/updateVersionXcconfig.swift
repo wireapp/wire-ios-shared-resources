1a0816f4b869663cd2aba64bf02a86aba2423aa9
Bill Chan <billypchan@gmail.com>
2021-06-15 22:16:12 +0200

    add updateVersionXcconfig.swift

---
+++ Scripts/updateVersionXcconfig.swift
@@ -0,0 +1,95 @@
+//
+// Wire
+// Copyright (C) 2021 Wire Swiss GmbH
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see http://www.gnu.org/licenses/.
+//
+
+/// Update Cartfile frameworks to latest version and write update content back to it.
+/// Example: $swift updateCatfile.swift zenkins:PAT /~/Documents/wire/wire-ios/Cartfile
+
+import Foundation
+
+//MARK: - main
+
+var secret: String?
+var path: String!
+var version: String!
+
+if CommandLine.arguments.count >= 4 {
+    secret = CommandLine.arguments[1]
+    path = CommandLine.arguments[2]
+    version = CommandLine.arguments[3]
+} else {
+    print("❌ exit: please provide secret and Version.xcconfig path.\nExample: $swift updateVersionXcconfig.swift zenkins:PAT ./Resources/Configurations/version.xcconfig 1.2.3")
+
+    exit(1)
+}
+
+let major = version.components(separatedBy: ".").first!
+
+let versionXcconfigUrl = URL(fileURLWithPath: path)
+
+print("ℹ️ updating: \(path!)")
+
+var lines: [String] = []
+
+do {
+    let contents = try String(contentsOf: versionXcconfigUrl, encoding: String.Encoding.utf8)
+    
+    lines = contents.components(separatedBy: "\n")
+    
+} catch {
+    print("❌ Error: \(error.localizedDescription)")
+    exit(1)
+}
+
+//MARK: request latest versions
+
+var updateString: String = ""
+
+lines.forEach() {
+    var components:[String] = $0.components(separatedBy: " ")
+    
+    if components.count >= 3 {
+        
+        let flag = components[0]
+        print("ℹ️ flag: \(flag)")
+        if flag == "MAJOR_VERSION" {
+            components[2] = major
+        } else if flag == "CURRENT_PROJECT_VERSION" {
+            components[2] = version
+        }
+    }
+
+    print("ℹ️ components: \n\(components)")
+    
+    updateString += components.joined(separator:" ") + "\n"
+}
+
+//MARK: update version
+
+
+print("ℹ️ updated Cartfile content: \n\(updateString)")
+
+//MARK: write to file
+
+print("✅ file written to: \(versionXcconfigUrl)")
+
+do {
+    try updateString.write(to: versionXcconfigUrl, atomically: false, encoding: .utf8)
+}
+catch {
+    print("❌ write error: \(error.localizedDescription)")
+}
