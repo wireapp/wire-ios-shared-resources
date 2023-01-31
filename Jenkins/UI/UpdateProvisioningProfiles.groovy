83dd71505c05c29f547fe4e5f0e73d07f6137ae1
François Benaiteau <francois.benaiteau@gmail.com>
2023-01-31 16:16:43 +0100

    remove unused variables

--- Jenkins/UI/UpdateProvisioningProfiles.groovy
+++ Jenkins/UI/UpdateProvisioningProfiles.groovy
@@ -1,12 +1,3 @@
-@NonCPS
-def getParentBuild() {
-    def upstream = currentBuild.rawBuild.getCause(hudson.model.Cause$UpstreamCause)
-    def upstreamJob = upstream?.upstreamProject ?: env.JOB_NAME
-    def upstreamNumber = upstream?.upstreamBuild ?: env.BUILD_NUMBER
-    return ["name":upstreamJob, "number":upstreamNumber]
-}
-def parentBuild = getParentBuild()
-
 pipeline {
     agent any
     triggers { 
@@ -16,16 +7,6 @@ pipeline {
         ansiColor('xterm')
     }
     environment { 
-        // This is where the misc configuration files (e.g. Gemfile) are fetched from. When testing swap out the branch
-        DEPENDENCIES_BASE_URL = "https://raw.githubusercontent.com/wireapp/wire-ios-shared-resources/master"
-        // For command line tools to be able to access API we set there env vars to values from Jenkins credentials store
-        GITHUB_TOKEN = credentials('github-api-token')
-        GITHUB_ACCESS_TOKEN = credentials('github-api-token')
-
-        // For uploading builds to S3 bucket
-        AWS_ACCESS_KEY_ID = credentials('s3_access_key')
-        AWS_SECRET_ACCESS_KEY = credentials('s3_secret_access_key')
-
         // For uploading to AppStore
         APPSTORE_CONNECT_USER = credentials('appstore_connect_username')
         APPSTORE_CONNECT_PASSWORD = credentials('appstore_connect_password')
@@ -38,18 +19,6 @@ pipeline {
 
         // Turn off annoying update checks
         FASTLANE_SKIP_UPDATE_CHECK = "YES"
-
-        // This will be set to app build number
-        BUILD_NUMBER = "${env.BUILD_NUMBER}"
-
-        // Repository from which to fetch custom AVS binary
-	AVS_REPO = "wireapp/avs-ios-binaries-appstore"
-
-        // DATADOG API
-        DATADOG_API_KEY = credentials('datadog_api_key')
-
-        XCODE_VERSION = "${xcode_version}"
-        CACHE_CARTHAGE = "${cache_carthage}"
     }
 	
     parameters {
