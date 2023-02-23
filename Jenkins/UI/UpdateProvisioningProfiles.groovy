e036a283ebb3748d92369ce74c33cf96b2918355
François Benaiteau <francois.benaiteau@gmail.com>
2023-02-23 18:28:45 +0100

    test

--- Jenkins/UI/UpdateProvisioningProfiles.groovy
+++ Jenkins/UI/UpdateProvisioningProfiles.groovy
@@ -10,7 +10,7 @@ pipeline {
         // For uploading to AppStore
         APPSTORE_CONNECT_USER = credentials('appstore_connect_username')
         APPSTORE_CONNECT_PASSWORD = credentials('appstore_connect_password')
-    
+        APPSTORE_CONNECT_FILE = credentials('app_store_connect_api_key')
         // For provisioning profiles update
         APPSTORE_CONNECT_TEAM_ID = credentials('appstore_connect_team_id')
 
@@ -39,68 +39,75 @@ pipeline {
 
 
     stages {
-
-        stage('build-assets & Gems') {
-	        parallel {
-	        	stage('checkout wire-ios-build-assets') {
-		            steps {
-		                dir("wire-ios-build-assets") {
-		                    checkout([
-		                        $class: 'GitSCM',
-		                        branches: [[name: '*/master']], // Checks out specified branch
-		                        extensions: [
-		                            [$class: 'LocalBranch', localBranch: '**'], // Unless this is specified, it simply checks out by commit SHA with no branch information
-		                            [$class: 'CleanBeforeCheckout'] // Resets untracked files, just to make sure we are clean
-		                        ],
-		                        userRemoteConfigs: [[url: "git@github.com:wireapp/wire-ios-build-assets.git"]]
-		                    ])
-		                }
-		            }
-	        	}
-
-	        	stage('Gems') {
-		            steps {
-		                sh """#!/bin/bash -l
-                            cd wire-ios-build-assets; bundle install --path ~/.gem
-                        """
-    	            }
-	            }
-	        }
-	    }
-
-        stage('Generate Provisioning Profiles') {
-            when {
-                anyOf {
-                    changeset "**/devices.txt"
-                    triggeredBy cause: "UserIdCause"
-                    branch "master"
-                } 
+        stage('debug') {
+            script {
+                sh """#!/bin/bash -l
+                    echo ${APPSTORE_CONNECT_FILE};
+                """
             }
-            steps {
-                withEnv([
-                    "FASTLANE_USER=${APPSTORE_CONNECT_USER}",
-                    "FASTLANE_PASSWORD=${APPSTORE_CONNECT_PASSWORD}",
-                    "FASTLANE_TEAM_ID=${APPSTORE_CONNECT_TEAM_ID}"
-                ]) {
-                    script {
-                        if (params.REGISTER_DEVICES) {
-                            sh """#!/bin/bash -l
-                            cd wire-ios-build-assets; bundle exec fastlane devices
-                            """
-                        } else {
-                            sh """#!/bin/bash -l
-                                cd wire-ios-build-assets; bundle exec fastlane profiles build_type:${BUILD_TYPE} configuration:${BUILD_CONFIGURATION} force:
-                            """
-                        }
-                    } 
-                }
-                
-            }   
-            post {
-                always {
-                    archiveArtifacts artifacts: 'wire-ios-build-assets/*.mobileprovision', fingerprint: true
-                }
-            }          
         }
+
+        // stage('build-assets & Gems') {
+	    //     parallel {
+	    //     	stage('checkout wire-ios-build-assets') {
+		//             steps {
+		//                 dir("wire-ios-build-assets") {
+		//                     checkout([
+		//                         $class: 'GitSCM',
+		//                         branches: [[name: '*/master']], // Checks out specified branch
+		//                         extensions: [
+		//                             [$class: 'LocalBranch', localBranch: '**'], // Unless this is specified, it simply checks out by commit SHA with no branch information
+		//                             [$class: 'CleanBeforeCheckout'] // Resets untracked files, just to make sure we are clean
+		//                         ],
+		//                         userRemoteConfigs: [[url: "git@github.com:wireapp/wire-ios-build-assets.git"]]
+		//                     ])
+		//                 }
+		//             }
+	    //     	}
+
+	    //     	stage('Gems') {
+		//             steps {
+		//                 sh """#!/bin/bash -l
+        //                     cd wire-ios-build-assets; bundle install --path ~/.gem
+        //                 """
+    	//             }
+	    //         }
+	    //     }
+	    // }
+
+        // stage('Generate Provisioning Profiles') {
+        //     when {
+        //         anyOf {
+        //             changeset "**/devices.txt"
+        //             triggeredBy cause: "UserIdCause"
+        //             branch "master"
+        //         } 
+        //     }
+        //     steps {
+        //         withEnv([
+        //             "FASTLANE_USER=${APPSTORE_CONNECT_USER}",
+        //             "FASTLANE_PASSWORD=${APPSTORE_CONNECT_PASSWORD}",
+        //             "FASTLANE_TEAM_ID=${APPSTORE_CONNECT_TEAM_ID}"
+        //         ]) {
+        //             script {
+        //                 if (params.REGISTER_DEVICES) {
+        //                     sh """#!/bin/bash -l
+        //                     cd wire-ios-build-assets; bundle exec fastlane devices
+        //                     """
+        //                 } else {
+        //                     sh """#!/bin/bash -l
+        //                         cd wire-ios-build-assets; bundle exec fastlane profiles build_type:${BUILD_TYPE} configuration:${BUILD_CONFIGURATION} force:
+        //                     """
+        //                 }
+        //             } 
+        //         }
+                
+        //     }   
+        //     post {
+        //         always {
+        //             archiveArtifacts artifacts: 'wire-ios-build-assets/*.mobileprovision', fingerprint: true
+        //         }
+        //     }          
+        // }
     }
 }
